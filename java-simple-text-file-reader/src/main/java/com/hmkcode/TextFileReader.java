9df8af28b52b16f6ca60ad8bf7e76871e0a51561
kavyav <kavyapvasista@gmail.com>
2015-05-06 11:38:21 +0530

    fixed#25

--- java-simple-text-file-reader/src/main/java/com/hmkcode/TextFileReader.java
+++ java-simple-text-file-reader/src/main/java/com/hmkcode/TextFileReader.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class TextFileReader {trrrrrrrrrrrrrrrrrr
 fdfddfdfffddf
-	private BufferedReadffffffder buffer;
+	private BufferedReadffffffder bufferghhhhhhhhhhhhh;
 	private String currentLine = "";sde
 	public TextFileReader(){ 
 		
